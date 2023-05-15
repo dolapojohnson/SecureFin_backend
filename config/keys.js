f28dfd50d1bcede1bbff42c631d682e54756ddc0
FORTUNE21 <awemary97@gmail.com>
2023-05-15 20:28:36 +0100

    Fixed API

--- config/keys.js
+++ config/keys.js
@@ -1,5 +1,5 @@
 module.exports = {
-  mongoURI: "mongodb://localhost:27017/secure_fin_backend",
-  // mongoURI: "mongodb+srv://dolapojohnson:Adewale97@securefin.byfrzaf.mongodb.net/?retryWrites=true&w=majority",
+  // mongoURI: "mongodb://localhost:27017/secure_fin_backend",
+  mongoURI: "mongodb+srv://dolapojohnson:Adewale97@securefin.byfrzaf.mongodb.net/?retryWrites=true&w=majority",
   secretOrKey: "secret",
 };
